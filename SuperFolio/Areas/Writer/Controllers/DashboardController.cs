904b58764d025300231f64052d38dd4f660cd28a
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-03-11 13:03:18 +0300

    Super Folio Portfölyo Projesi
    Bölüm 21: Webden Api ile Veri Alma
    Dashboard For Writer,
    Giriş Yapan Kullanıcının Ad Soyad Bilgilerini Getirme,
    Yazar Dashboard İstatistik,
    Api ile Hava Durumu Bilgisi Çekme,
    Yazar Layout,
     Layout Bildirim Ayarı,
    Bildirimlerin Listelenmesi,
    Oturum Açan Kullanıcı Resmi

---
+++ SuperFolio/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SuperFolio.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.UserName = values.Name + " " + values.Surname;
+
+            //weather api
+            string api = "dd6e5716727e03344c38dfb6a2419c27";
+            string connection = " http://api.openweathermap.org/data/2.5/weather?q=mardin&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.Weather = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+
+            //statistics
+            Context context = new Context();
+            ViewBag.MessageCount = 0;
+            ViewBag.NotificationCount = context.Announcements.Count();
+            ViewBag.TotalUserCount = context.Users.Count();
+            ViewBag.TotalSkillCount = context.Skills.Count();
+
+            return View();
+        }
+    }
+}
+/*
+ http://api.openweathermap.org/data/2.5/weather?q=mardin&mode=xml&lang=tr&units=metric&appid=dd6e5716727e03344c38dfb6a2419c27
+ */
---
+++ SuperFolio/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,22 @@
+﻿using BusinnesLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace SuperFolio.Areas.Writer.ViewComponents
+{
+    public class Notification : ViewComponent
+    {
+        private readonly IAnnouncementService _announcementService;
+
+        public Notification(IAnnouncementService announcementService)
+        {
+            _announcementService = announcementService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _announcementService.TGetList().Take(5).OrderByDescending(x => x.AnnouncementID);
+            return View(values);
+        }
+    }
+}
