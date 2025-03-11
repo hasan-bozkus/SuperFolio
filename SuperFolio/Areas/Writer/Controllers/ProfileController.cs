11093fd1100f241c30d6f2ca4ef013ab22640ff7
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-03-11 10:27:13 +0300

    Super Folio Portfölyo Projesi
    Bölüm 20: Profil Sayfası
    Profile Sayfası,
    Profile User Manager ile Bilgileri Getirme,
    User Edit View Model,
    Update User Controller,
    Update User View Model,
    Update İşlemi,
    Dosyadan Resim Yükleme

---
+++ SuperFolio/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,60 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SuperFolio.Areas.Writer.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SuperFolio.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureUrl = values.ImageUrl;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel userEditViewModel)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (userEditViewModel.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extehsions = Path.GetExtension(userEditViewModel.Picture.FileName);
+                var imagename = Guid.NewGuid() + extehsions;
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;
+                var stream = new FileStream(savelocation, FileMode.Create);
+                await userEditViewModel.Picture.CopyToAsync(stream);
+                user.ImageUrl = imagename;
+            }
+
+            user.Name = userEditViewModel.Name;
+            user.Surname = userEditViewModel.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
