0041a770a8e37b36e2000157789e4aefd57bfc2a
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-03-22 11:38:45 +0300

    Super Folio Portfölyo Projesi
    Bölüm 29: Apiler
    Apiler,
    Apinin Ayağa Kaldırılması,
    Api Projesi İçin Db Oluşturma,
    Apilerle Get İsteği,
    Apilerde ID'ye Göre Get,
    Apiler Üzerinden Ekleme,
    Apiler Üzerinden Silme,
    Apiler Üzerinden Güncelleme

---
+++ SuperFolio.Api/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperFolio.Api.DAL.ApiContext;
+using SuperFolio.Api.DAL.Entity;
+using System.Linq;
+
+namespace SuperFolio.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            using var c = new Context();
+
+            var values = c.Categories.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            using var c = new Context();
+            var value = c.Categories.Find(id);
+            if (value == null)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            using var c = new Context();
+            c.Categories.Add(category);
+            c.SaveChanges();
+            return Created("Kategori başarılı bir şekilde eklendi.", category);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            using var c = new Context();
+            var value = c.Categories.Find(id);
+            if (value == null)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+            c.Categories.Remove(value);
+            c.SaveChanges();
+            return Ok("Kategori başarılı bir şekilde silindi.");
+        }
+
+        [HttpPut]
+        public IActionResult EditCategory(Category category)
+        {
+            using var c = new Context();
+            var value = c.Find<Category>(category.CategoryID);
+            if (value == null)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+            value.CategoryName = category.CategoryName;
+            c.Categories.Update(value);
+            c.SaveChanges();
+            return Ok("Kategori başarılı bir şekilde eklendi.");
+        }
+    }
+}
---
+++ SuperFolio.Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperFolio.Api.DAL.Entity;
+
+namespace SuperFolio.Api.DAL.ApiContext
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\hasan;Database=SuperFolioApiDB; Integrated security=true; Trusted_Connection=True;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
